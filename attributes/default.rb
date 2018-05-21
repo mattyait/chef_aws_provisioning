7f917f7ae7d1c89095d8ec97fef9b5120480317c
matty <mattyait@gmail.com>
2018-05-21 22:20:41 +1000

    Adding attributes for recipes

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['chef_aws_provisioning']['aws']['profile']='default'
+default['chef_aws_provisioning']['aws']['region'] = 'us-east-1'
+default['chef_aws_provisioning']['vpc']['vpc_name'] = 'demo'
+default['chef_aws_provisioning']['vpc']['cidr_block'] = '198.0.0.0/24'
--- recipes/vpc.rb
+++ recipes/vpc.rb
@@ -4,14 +4,14 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
-require 'chef/provisioning/aws_driver'   
-with_driver 'aws::us-east-1'             
-                                         
-aws_vpc 'chef_demo' do                   
-  cidr_block '198.0.0.0/24'              
-  internet_gateway true                  
-  instance_tenancy :default              
-  enable_dns_support true                
-  enable_dns_hostnames true              
-  aws_tags :chef_type => 'aws_vpc'       
-end                                      
+require 'chef/provisioning/aws_driver'
+with_driver 'aws:' + node['chef_aws_provisioning']['aws']['profile'] + ':' + node['chef_aws_provisioning']['vpc']['aws_region']
+
+aws_vpc node['chef_aws_provisioning']['vpc']['vpc_name'] do
+  cidr_block node['chef_aws_provisioning']['vpc']['cidr_block']
+  internet_gateway true
+  instance_tenancy :default
+  enable_dns_support true
+  enable_dns_hostnames true
+  aws_tags :chef_type => 'aws_vpc'
+end
